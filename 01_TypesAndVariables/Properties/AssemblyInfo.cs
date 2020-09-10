50a1c48ecb81255d565a95fa5a09e20c24b7eed7
aknight9255 <amanda.knight55@gmail.com>
2020-09-10 15:32:37 -0400

    Add project files.

---
+++ 01_TypesAndVariables/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("01_TypesAndVariables")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("01_TypesAndVariables")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("525be156-98ad-450e-8bd4-0e938a236e44")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 01_TypesAndVariables/ValueTypeExamples.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _01_TypesAndVariables
+{
+    [TestClass]
+    public class ValueTypeExamples
+    {
+        [TestMethod]
+        public void Booleans()
+        {
+            bool isDeclared;
+            isDeclared = true;
+
+            bool isDeclarationAndInitialized = false;
+            isDeclarationAndInitialized = true;
+        }
+
+        [TestMethod]
+        public void Characters()
+        {
+            char character = 'a';
+            char symbol = '?';
+            char number = '7';
+            char space = ' ';
+            char specialCharacter = '\n';
+        }
+
+        [TestMethod]
+        public void WholeNumbers()
+        {
+            byte byteExample = 255;
+            sbyte sbyteExample = -128;
+            short shortExample = 32767;
+            Int16 anotherShortExample = 32000;
+            int intMin = -2147483648;
+            Int32 intMax = 2147483647;
+            long longExample = 9223372036854775807;
+            Int64 longMin = -9223372036854775808;
+
+            int a = 42;
+            int b = 20;
+
+            byte age = 87;
+        }
+
+        [TestMethod]
+        public void Decimals()
+        {
+            float floatExample = 1.045231f;
+            double doubleExample = 1.7890532789070364095830948509384509384509d;
+            decimal decimalExample = 1.2578907289045789789789789787897m;
+
+            Console.WriteLine(1.2578907289045789789789789787897f);
+            Console.WriteLine(1.2578907289045789789789789787897d);
+            Console.WriteLine(1.2578907289045789789789789787897m);
+        }
+
+        enum PastryType { Cake, Cupcake, Croissant, Poptart, Pie, Donut};
+
+        [TestMethod]
+        public void Enums()
+        {
+            PastryType myPastry = PastryType.Croissant;
+            PastryType anotherOne = PastryType.Poptart;
+        }
+
+        [TestMethod]
+        public void Structs()
+        {
+            Int32 age = 112;
+
+            DateTime today = DateTime.Today;
+            Console.WriteLine(today);
+            DateTime birthday = new DateTime(1805,12,03);
+        }
+    }
+}
