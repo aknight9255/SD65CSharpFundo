fba8a3d459bf2c50dfbe86ba6417bc6f5c5d6015
aknight9255 <amanda.knight55@gmail.com>
2020-09-14 10:18:04 -0400

    Monday Morning Lecture

--- 03_Classes/Person.cs
+++ 03_Classes/Person.cs
@@ -20,6 +20,25 @@ namespace _03_Classes
 		public DateTime DateOfBirth { get; set; }
 		public Vehicle Transport { get; set; }
 
+		public string FullName
+		{
+			get
+			{
+				return $"{FirstName} {LastName}";
+			}
+		}
+
+		public int Age
+		{
+			get
+			{
+				TimeSpan ageSpan = DateTime.Now - DateOfBirth;
+				double totalAgeInYears = ageSpan.TotalDays / 365.25;
+				int yearsOfAge = Convert.ToInt32(Math.Floor(totalAgeInYears));
+				return yearsOfAge;
+			}
+		}
+
 		public Person() { }
 		public Person(string firstName, string lastName, DateTime dob,Vehicle transport)
 		{
--- 03_Classes/PersonTests.cs
+++ 03_Classes/PersonTests.cs
@@ -10,6 +10,17 @@ namespace _03_Classes
         public void TestMethod1()
         {
             Person firstPerson = new Person("Howard", "Stefanopoulous", new DateTime(1785,02,14), new Vehicle());
+            Console.WriteLine(firstPerson);
+            Console.WriteLine(firstPerson.Age);
+            int anotherAge = firstPerson.Age;
+            firstPerson.Transport.Make = "A New Car!";
+            firstPerson.Transport.Model = "Blue";
+            firstPerson.Transport.Mileage = 2;
+            firstPerson.Transport.TypeOfVehicle = VehicleType.FlyingBison;
+
+            Assert.AreEqual("Howard Stefanopoulous", firstPerson.FullName);
+            
+
         }
     }
 }
