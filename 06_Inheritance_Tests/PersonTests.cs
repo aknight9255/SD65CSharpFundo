dc1dd2f221f8dfe4346dc2d342093dfc1b6cbc89
aknight9255 <amanda.knight55@gmail.com>
2020-09-15 14:41:56 -0400

    after lunch tuesday

--- 06_Inheritance_Tests/PersonTests.cs
+++ 06_Inheritance_Tests/PersonTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using _06_Inheritance_Classes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -14,6 +15,60 @@ namespace _06_Inheritance_Tests
             martha.SetFirstName("Martha");
             martha.SetLastName("Vineyard");
             Console.WriteLine(martha.Name);
+
+            Customer bob = new Customer();
+            bob.SetFirstName("Bobert");
+            bob.SetLastName("Boss");
+
+            SalaryEmployee tedEmployee = new SalaryEmployee
+            {
+                PhoneNumber = "1800-fakenumber",
+                Salary = 800000,
+                HireDate = new DateTime(1304, 01, 01),
+                EmployeeNumber = 394
+            };
+            Console.WriteLine(tedEmployee.YearsWithCompany);
+
+        }
+
+        [TestMethod]
+        public void EmployeeTestsAKAStarkTower()
+        {
+            Employee jarvis = new Employee();
+            HourlyEmployee tony = new HourlyEmployee();
+            SalaryEmployee pepper = new SalaryEmployee();
+            tony.HoursWorked = 55;
+            tony.HourlyWage = 3000;
+            pepper.Salary = 200000;
+            HourlyEmployee peter = new HourlyEmployee();
+            SalaryEmployee happy = new SalaryEmployee();
+            happy.Salary = 150000;
+            List<Employee> allEmployees = new List<Employee>();
+            jarvis.SetFirstName("Jarvis");
+            tony.SetFirstName("Tony");
+            pepper.SetFirstName("Pepper");
+            peter.SetFirstName("Peter");
+            happy.SetFirstName("Happy");
+            allEmployees.Add(jarvis);
+            allEmployees.Add(tony);
+            allEmployees.Add(pepper);
+            allEmployees.Add(peter);
+            allEmployees.Add(happy);
+
+
+            foreach(Employee worker in allEmployees)
+            {
+                
+                if(worker.GetType() == typeof(SalaryEmployee))
+                {
+                    SalaryEmployee sEmployee = (SalaryEmployee)worker;
+                    Console.WriteLine($"This is a salary employee that makes {sEmployee.Salary}");
+                }
+                else if (worker is HourlyEmployee hourlyWorker)
+                {
+                    Console.WriteLine($"{worker.Name} has worked {hourlyWorker.HoursWorked} hours!");
+                }
+            }
         }
     }
 }
