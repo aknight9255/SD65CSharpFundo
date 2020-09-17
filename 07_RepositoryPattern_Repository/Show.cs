999acf955cc92cb0b74588c07464d910995ac868
aknight9255 <amanda.knight55@gmail.com>
2020-09-17 10:32:16 -0400

    Thursday Morning Notes

---
+++ 07_RepositoryPattern_Repository/Show.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_RepositoryPattern_Repository
+{
+    public class Show : StreamingContent
+    {
+        public int SeasonCount { get; set; }
+        public int EpisodeCount { get; set; }
+        public double AverageRunTime { get; set; }
+        public List<Episode> Episodes { get; set; } = new List<Episode>();
+
+        //public Show()
+        //{
+        //    Episodes = new List<Episode>();
+        //}
+    }
+    public class Episode
+    {
+        public string Title { get; set; }
+        public double RunTime { get; set; }
+        public int SeasonNumber { get; set; }
+    }
+}
---
+++ 07_RepositoryPattern_Tests/StreamingContentTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using _07_RepositoryPattern_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _07_RepositoryPattern_Tests
+{
+    [TestClass]
+    public class StreamingContentTests
+    {
+        [TestMethod]
+        public void StreamingContentNotes()
+        {
+            StreamingContent baseObject = new StreamingContent();
+            Movie movieObject = new Movie();
+            Show showObject = new Show();
+            Episode episodeObject = new Episode();
+            showObject.Episodes = new List<Episode>();
+            showObject.Episodes.Add(episodeObject);
+            Movie newMovie = new Movie("Venom", "The best romance movie of our age", 9005, "pg-13", true, GenreType.RomCom, 132);
+
+        }
+    }
+}
